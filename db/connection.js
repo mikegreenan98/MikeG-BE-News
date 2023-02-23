afe568ad5b91dcf8b40e3e73f0b7d5ddd3ffff54
Michael Greenan <mikegreenan98@gmail.com>
2023-02-23 14:43:38 +0000

    14-hosting app is done

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,12 @@
 const { Pool } = require('pg');
-const { connectionString } = require('pg/lib/defaults');
 
-// if jest is running, process.env.NODE_ENV is set to "test"
-// so ENV is set to 'test' if it exists (via jest), otherwise is ENV set to 'development'
+// if jest is running, NODE_ENV is set to "test".
+// If in production, we must first run the seed-prod script as defined in package.json,
+// which sets NODE_ENV=production before running seed.
+// so ENV is set to 'test' or production if they are defined, otherwise ENV is set to 'development'
 const ENV = process.env.NODE_ENV || 'development';
 
 
-// If in production stage, need to set a 'config' object that is used by hosted db on Elephant
-// TBD - MIKE - UPDATES THIS COMMENT WHEN FIGURE OUT HOW DATABASE_URL GETS SET VIA .env.production
-const config = ENV === 'production' ?
-                {connectionString: process.env.DATABASE_URL,
-                  max: 2, } :
-                {};
 
 // this uses ENV to get PGDATABASE variable from either .env.test, or .env.development
 // dotenv it a tool that loads environmental variables into proces.env
@@ -23,5 +18,11 @@ require('dotenv').config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
+
+// If in production stage, need to set a 'config' object that is used by hosted db on Elephant
+const config = ENV === 'production' ?
+                {connectionString: process.env.DATABASE_URL,
+                  max: 2, } :
+                {};
 
 module.exports = new Pool(config);
